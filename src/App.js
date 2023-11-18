7e45f10380597de84be28dc79bc8e152d170b0cb
Corruptbyte <130069761+Corruptbyte@users.noreply.github.com>
2023-11-17 19:41:52 -0600

    App.js

--- src/App.js
+++ src/App.js
@@ -1,78 +1,52 @@
 import './App.css';
 // import Item from './MyItem'
-
-
 import React from 'react';
 class Pokemon extends React.Component {
-
-    constructor() {
-
-   super()
-
- this.state = {
-
-    loadedCharacter: false,
-   name: null,
-  height: null,
- species: null,
-moves: [],
-
-     }
-  }
-      getNewCharacter() {
-
-
-  console.log("Get new character from a button")
-      
-
-     this.setState({
-
-
-  name: 'Charizard',
-  height: 170,
-  species: 'salamander',
-  moves: [],
-  loadedCharacter: true,
-
-
-      })
-
-    }
-
-
-    render() {
-    return (
-    <div>
-
+        constructor() {
+            super()
+            this.state = {
+                loadedCharacter: false,
+                name: null,
+                height: null,
+                species: null,
+                moves: [],
+            }
+        }
+        getNewCharacter() {
+            console.log("Get new character from a button")
+            this.setState({
+                name: 'Charizard',
+                height: 170,
+                species: 'salamander',
+                moves: [],
+                loadedCharacter: true,
+            })
+        }
+        render() {
+                return (<div>
 {
 
-       this.state.loadedCharacter &&
+this.state.loadedCharacter &&
 
-     <div>
+<div>
 
 <h1>{this.state.name}</h1>
 <p>{this.state.height} cm</p>
 <p>{this.state.species}:</p>
- <ul>
-   <li>{this.state.moves}</li>
- </ul>
+<ul>
+<li>{this.state.moves}</li>
+</ul>
 </div>
-       }
-
-
+}
 <button type="button" onClick={() => this.getNewCharacter()}className="btn">Randomized character</button>
-     )
-    }
-  }
+)
+}
+}
 
-     function App() {
-  
-
-        return (
-      <div className="App">
-      
-<header className="App-header">
-
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
       <Pokemon />
       
         
