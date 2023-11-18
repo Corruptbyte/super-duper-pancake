838adf7a748b5736ecffc456925ac3911a418db9
Corruptbyte <130069761+Corruptbyte@users.noreply.github.com>
2023-11-18 00:37:53 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
 import './App.css';
+// import Item from './MyItem'
+
+
+import React from 'react';
+class Pokemon extends React.Component {
+constructor() {
+super()
+this.state = {
+loadedCharacter: false,
+name: null,
+height: null,
+species: null,
+moves: [],
+}
+}
+getNewCharacter() {
+console.log("Get new character from a button")
+this.setState({
+name: 'Charizard',
+height: 170,
+species: 'salamander',
+moves: [],
+loadedCharacter: true,
+})
+}
+render() {
+return (
+<div>
+{
+
+this.state.loadedCharacter &&
+
+<div>
+
+<h1>{this.state.name}</h1>
+<p>{this.state.height} cm</p>
+<p>{this.state.species}:</p>
+<ul>
+<li>{this.state.moves}</li>
+</ul>
+</div>
+}
+<button type="button" onClick={() => this.getNewCharacter()}className="btn">Randomized character</button>
+)
+}
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+      <Pokemon />
+      
+        
+    
       </header>
     </div>
   );
 }
-
 export default App;
